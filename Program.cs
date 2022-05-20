e32e9fc16d933e0a2bb605b3b63b5209da0bd9f3
9918005084U <umeshchandrareddyaenike@gmail.com>
2022-05-20 20:55:08 +0530

    first

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            Console.WriteLine("Enter  the mark");
+            score = Convert.ToInt32(Console.ReadLine());
+            if (score > 90)
+            {
+                Console.WriteLine("Excellent");
+
+            }
+            else if (score < 90 && score >= 80)
+            {
+                Console.WriteLine("Very Good");
+            }
+            else if (score <80 && score >=70)
+            {
+                Console.WriteLine("Fair");
+            }
+            else if (score <70 && score >=60)
+            {
+                Console.WriteLine("not bad");
+            }
+            else
+            {
+                Console.WriteLine("bad");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
